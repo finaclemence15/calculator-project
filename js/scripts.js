592369e91e55270073528434baf575a1ab1cae15
finaclemence15 <finaclemence15@gmail.com>
2019-07-16 15:18:41 +0200

    division

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,19 @@
 //   var number2 = parseInt(prompt("Enter another number:"));
 //   var result = subtract(number1, number2);
 //   alert(result);
-var multiply = function(number1, number2) {
-    return number1 * number2;
+// var multiply = function(number1, number2) {
+//     return number1 * number2;
+//   };
+  
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = multiply(number1, number2);
+//   alert(result);
+var divid = function(number1, number2) {
+    return number1 / number2;
   };
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  var result = multiply(number1, number2);
+  var result = divid(number1, number2);
   alert(result);
